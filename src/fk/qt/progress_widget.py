b26cdb311e0694625fb5005ed72f410476771bd2
Christophe Gautherat <christophe.gautherat@orange.com>
2025-02-19 21:56:38 +0100

    Enhance progress widget with items count and non started pomodoros of started items

--- src/fk/qt/progress_widget.py
+++ src/fk/qt/progress_widget.py
@@ -13,6 +13,8 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import logging
+
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel
 
 from fk.core.abstract_event_source import AbstractEventSource
@@ -44,17 +46,38 @@ class ProgressWidget(QWidget):
         source.on("AfterPomodoro*", lambda workitem, **kwargs: self.update_progress(workitem.get_parent()))
 
     def update_progress(self, backlog_or_tag: Backlog | Tag | None) -> None:
-        total: int = 0
-        done: int = 0
+        total_items: int = 0
+        done_items: int = 0
+        total_pomodoros: int = 0
+        done_pomodoros: int = 0
         if backlog_or_tag:
             workitems = backlog_or_tag.values() if type(backlog_or_tag) is Backlog else backlog_or_tag.get_workitems()
-            for wi in workitems:
-                for p in wi.values():
-                    total += 1
-                    if p.is_finished() or p.is_canceled():
-                        done += 1
 
-        self.setVisible(total > 0)
-        self._label.setVisible(total > 0)
-        percent = f' ({round(100 * done / total)}%)' if total > 0 else ''
-        self._label.setText(f'{done} of {total} done{percent}')
+            for wi in workitems:
+                # Non startable workitems and not finished are not counted (they don't have any pomodoros, meaning they are not planned)
+                if not wi.is_startable() and not wi.is_sealed() and not wi.is_running():
+                    continue
+                total_items += 1
+                # sealed items are counted as done
+                if wi.is_sealed():
+                    done_items += 1
+
+                for p in wi.values():
+                    # TODO: activate with a configuration ?
+                    # # We do not count non started pomodoros of sealed workitems
+                    # if wi.is_sealed() and p.is_startable():
+                    #     print(" ➡️ ignored", end="")
+                    #     continue
+                    total_pomodoros += 1
+                    if p.is_finished() or p.is_canceled():
+                        done_pomodoros += 1
+                    # We count non started pomodoros of sealed workitems as done
+                    if wi.is_sealed() and p.is_startable():
+                        done_pomodoros += 1
+
+        self.setVisible(total_pomodoros > 0 or total_items > 0)
+        self._label.setVisible(total_pomodoros > 0 or total_items > 0)
+        percent_items = f' ({round(100 * done_items / total_items)}%)' if total_items > 0 else ''
+        percent_pomodoros = f' ({round(100 * done_pomodoros / total_pomodoros)}%)' if total_pomodoros > 0 else ''
+        self._label.setText(f'✔️ planned items : {done_items} of {total_items} done{percent_items} - '
+                            f'🍅 pomodoros : {done_pomodoros} of {total_pomodoros} done{percent_pomodoros}')
